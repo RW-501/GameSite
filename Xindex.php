1198881e2c1cf57576f98e02bf244f5260d6455a
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-04-26 19:34:59 -0500

    Add files via upload

---
+++ Xindex.php
@@ -0,0 +1,155 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<!-- saved from url=(0033)https://www.blackpeoplemarketplace.com -->
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type" />
+
+<title>Spades | </title>
+<meta name="description" content= ""/>
+<meta name="keywords" content= />
+
+
+<meta name="Distribution" content="Global" />
+
+<meta name="Rating" content="General" />
+
+<meta name="Robots" content="INDEX,FOLLOW" />
+
+<meta name="Revisit-after" content="1 Day" />
+	
+	
+	
+
+<style type="text/css" media="all">
+
+<!--
+
+@import url("../GameSite/index/backend/indexCSS.css");
+
+
+
+
+-->
+
+</style>
+
+</head>
+	<?php
+	
+$path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/GameSite/Includes/Scripts/Header/index.php";
+   include($path); ?>
+	
+<body>
+	
+		
+	<div id='page_container'>
+
+		
+		
+		<div id='mainCardTable'>
+		
+		
+		
+		<div id='topPlayer'>
+			
+			
+			</div>
+			
+			
+					<div id='tableMiddle'>
+			
+			
+		
+					<div id='leftPlayer'>
+			
+			
+			</div>
+					<div id='tableCenter'>
+			
+			
+			</div>
+					<div id='rightPlayer'>
+			
+			
+			</div>
+			</div>
+			
+			
+				<div id='mainPlayer'>
+			
+			
+					<div id='playerCard_0'>
+			
+			
+			</div>
+					
+					
+					
+			</div>
+		
+		
+		
+		
+		
+		</div>
+
+	
+			<button id='startGame'>Start</button>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+			<?php /*
+$path = $_SERVER['DOCUMENT_ROOT'];
+		
+		$path .= "/GameSite/Includes/Scripts/Footer/index.php";
+		 // echo($path);
+
+   include($path);*/ ?>
+	</div>
+		
+		
+</body>
+</html>
